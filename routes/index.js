63101ff917248e4aa6c4bd03cdc2647c7fbc4071
SkydersZ <sofiane.boucard@laposte.net>
2020-04-17 14:59:33 +0200

    feature(route): Ajout du paramètre de recherche.
    * Ajout du paramètre de recherche dans la route `index.js`.

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,26 @@ const express = require('express');
 const router = express.Router();
 const client = require('../instances/elasticsearch');
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'Express'});
 });
 
 /* GET elastic search results */
-router.get('/search', function(req, res) {
+router.get('/search/:searchValue', function(req, res) {
+  const searchValue = req.params.searchValue;
+  let resultsArray = [];
+
+  // Cette vérification est pour l'instant inutile
+  if (searchValue === '') {
+    res.status(404).send('Not found');
+    return;
+  }
+
   const bodyContent = {
     query: {
       match: {
-        content: 'Avril',
+        content: searchValue,
       },
     },
   };
@@ -21,6 +29,8 @@ router.get('/search', function(req, res) {
   client.search({index: 'search', body: bodyContent})
       .then((results) => {
         console.log(`found ${results.hits.total} items in ${results.took}ms`);
+        // Mettre les éléments du résults trouvés dans le cache.
+        resultsArray = results.hits;
         res.send(results.hits.hits);
       }).catch((err)=>{
         console.log(err);
