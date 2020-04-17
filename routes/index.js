e383e344caeea4a18b3e3ae7ec1b8a6d6bd06e2c
SkydersZ <sofiane.boucard@laposte.net>
2020-04-17 14:12:47 +0200

    feature(app): Liaison de nodejs avec elasticsearch
    * Ajout de la référence entre nodejs et elasticseach
    
    * Création d'un dossier `instances` contenant toutes les instances
      utilisés dans nodejs.
    
    * Ajout du package npm `elasticsearch.js` dans la solution
      elasticseach.js.
    
    * Ajout d'une nouvelle route dans le index.js.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const client = require('../instances/elasticsearch');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {title: 'Express'});
+});
+
+/* GET elastic search results */
+router.get('/search', function(req, res) {
+  const bodyContent = {
+    query: {
+      match: {
+        content: 'Avril',
+      },
+    },
+  };
+
+  client.search({index: 'search', body: bodyContent})
+      .then((results) => {
+        console.log(`found ${results.hits.total} items in ${results.took}ms`);
+        res.send(results.hits.hits);
+      }).catch((err)=>{
+        console.log(err);
+        res.send([]);
+      });
 });
 
 module.exports = router;
